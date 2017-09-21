4b8cf51595541c58188e504d5d23ad71688855a3
owen-carter <owenjiao@outlook.com>
2017-09-21 14:04:23 +0800

    update the start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #! /usr/bin/env bash
 # author : owen-carter
 
-printf "start to deploy spider-mp4ba project..."
-printf "you must installed the node and git"
-git clone git clone git@github.com:owen-carter/spider-mp4ba.git
+echo "start to deploy spider-mp4ba project..."
+echo "you must installed the node and git"
+git clone https://github.com/owen-carter/spider-mp4ba.git
 cd spider-mp4ba
 npm install
-printf "deploy the spider success!"
+echo "deploy the spider success!"
 npm run app
 
 
